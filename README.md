c39b38439186069b4b76f2b4eb8ff6768f949d4f
nrlchow <nrlchow@gmail.com>
2017-09-24 15:36:26 +1000

    Update project

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # Semantic Segmentation
-### Introduction
-In this project, I'll label the pixels of a road in images using a Fully Convolutional Network (FCN).
 
-### Setup
-##### Frameworks and Packages
-Make sure you have the following is installed:
- - [Python 3](https://www.python.org/)
- - [TensorFlow](https://www.tensorflow.org/)
- - [NumPy](http://www.numpy.org/)
- - [SciPy](https://www.scipy.org/)
+In this project, I have labelled the pixels of a road in images using a Fully Convolutional Network (FCN). 
+I've used the pretrained vgg16 model and trained the model to learn the correct features from the images. 
+I have replaced fully connected layers with the 1x1 convolutional layers then added up-sampling by using transposed convolutional layers and 
+skip connections to get better segmenatation results. I have tuned hyperparameters to optimize the neural network model. 
+I have set the number of epochs and batch sizes to 100 and 5 respectively.
+While training the model,I have observed decreased loss over time. 
+Once the model built,it has generated inference images on Kitti Road dataset with correct label.
+
+![a01](./runs/a01.png)
+
+![a1](./runs/a1.png)
+
+![a2](./runs/a2.png)
+
+![a03](./runs/a03.png)
+
+![a3](./runs/a3.png)
+
+![a4](./runs/a4.png)
+
+![a5](./runs/a5.png)
+
+![a7](./runs/a7.png)
+
+![a8](./runs/a8.png)
+
+Image : The loss of the network while the network is training - 
+
+
+![trainingLoss](./networkTraining.png)
+
+
  
-
--- main.py
+++ main.py
@@ -32,8 +32,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
+    image_input = sess.graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = sess.graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = sess.graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = sess.graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = sess.graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,8 +57,38 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+
+    def conv_1x1(input, num_classes):
+        kernel_size = 1
+        stride = 1
+        return tf.layers.conv2d(input, num_classes, kernel_size, stride)
+
+
+    # Apply 1x1 convolution to vgg layer 7 out
+
+    conv_out = conv_1x1(vgg_layer7_out, num_classes,)
+
+    # Transpose convolutions and upscale by 2
+
+    deconv_1 = tf.layers.conv2d_transpose(conv_out, num_classes, 4, 2, 'SAME')
+
+    # Add Pool 4 skip connection to previous VGG layer and Upscale by 2
+
+    skip_layer_Pool4 = conv_1x1(vgg_layer4_out, num_classes)
+    skip_conn_Pool4 = tf.add(deconv_1, skip_layer_Pool4)
+    deconv_2 = tf.layers.conv2d_transpose(skip_conn_Pool4, num_classes, 4, 2, 'SAME')
+
+    # Add Pool 3 skip connection to previous VGG layer and Upscale by 2
+
+    skip_layer_2_Pool3 = conv_1x1(vgg_layer3_out, num_classes)
+    skip_conn_Pool3 = tf.add(deconv_2, skip_layer_2_Pool3)
+
+    deconv_3 = tf.layers.conv2d_transpose(skip_conn_Pool3, num_classes, 16, 8, 'SAME')
+
+    return deconv_3
+
 tests.test_layers(layers)
+
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -61,11 +101,27 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+
+    # Reshape NN output from 4D to 2D (logits and labels)
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+
+    # Define loss
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+
+    # Define optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+    # Define train_op to minimise loss
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
+
+
 tests.test_optimize(optimize)
 
 
-def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss,input_image,
              correct_label, keep_prob, learning_rate):
     """
     Train neural network and print out the loss during training.
@@ -81,15 +137,33 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
-tests.test_train_nn(train_nn)
 
+    keep_prob_stat = 0.8
+    learning_rate_stat = 1e-4
+
+    for epoch in range(epochs):
+        for batch_images, batch_labels in get_batches_fn(batch_size):
+            feed_dict = {input_image: batch_images,
+                         correct_label: batch_labels,
+                         keep_prob: keep_prob_stat,
+                         learning_rate: learning_rate_stat}
+            _, loss = sess.run([train_op, cross_entropy_loss],feed_dict=feed_dict)
+        print("Epoch %d of %d: Training loss: %.4f" % (epoch + 1, epochs, loss))
+
+tests.test_train_nn(train_nn)
 
 def run():
     num_classes = 2
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
+    tests.test_for_kitti_dataset(data_dir)
+
+    epochs = 100
+    batch_size = 5
+
+    #tests.test_layers(layers)
+    tests.test_optimize(optimize)
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -108,15 +182,32 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # Build NN using load_vgg, layers, and optimize function
 
-        # TODO: Train NN using the train_nn function
+        input_image,keep_prob, layer_3_out, layer_4_out, layer_7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer_3_out,layer_4_out,layer_7_out,num_classes)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        correct_label = tf.placeholder(dtype = tf.float32,shape=(None,None,None,num_classes))
+        learning_rate = tf.placeholder(dtype=tf.float32,shape=(None))
+
+        logits,train_op,cross_entropy_loss = optimize(layer_output,correct_label,learning_rate,num_classes)
+
+        # Train NN using the train_nn function
+
+        sess.run(tf.global_variables_initializer())
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+
+
+        # Save inference data using helper.save_inference_samples
+
+        saver = tf.train.Saver()
+        saver.save(sess,'./checkpoints/model1.ckpt')
+        saver.export_meta_graph('./checkpoints/model/meta')
+        tf.train.write_graph(sess.graph_def,'./checkpoints/','model1.pb',False)
+        helper.save_inference_samples(runs_dir,data_dir,sess,image_shape,logits,keep_prob,input_image)
+        print("Model saved")
 
         # OPTIONAL: Apply the trained model to a video
-
 
 if __name__ == '__main__':
     run()
